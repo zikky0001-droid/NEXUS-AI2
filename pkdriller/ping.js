441464c85aee0659ec201391c69bca760765152d
Mejja <pkdrillerbusiness@gmail.com>
2025-06-29 07:32:33 +0300

    Update ping.js

--- pkdriller/ping.js
+++ pkdriller/ping.js
@@ -55,7 +55,7 @@ zokou({ nomCom: "ping", categorie: "General" }, async (dest, zk, commandeOptions
     }, { quoted: ms });
 
     await zk.sendMessage(dest, {
-        text: ",,,Code is poetry for machines, and innovation is its rhythm.,,,"
+        text: ",,,🪀fast★★★★★,,,"
     } ,{ quoted: ms });// Voice note form
     }catch (e) {
         console.log("❌ Ping Command Error: " + e);
