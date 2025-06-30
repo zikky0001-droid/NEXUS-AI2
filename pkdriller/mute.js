38a922878372f94d5dd26255a021ceaf3adb1666
Mejja <pkdrillerbusiness@gmail.com>
2025-06-30 05:55:06 +0300

    Update mute.js

--- pkdriller/mute.js
+++ pkdriller/mute.js
@@ -1,39 +1,57 @@
-const { zokou } = require('../framework/zokou'); 
-const {muteUser, unmuteUser, isUserMuted} = require('../bdd/mute') 
-const s = require("../set")
+const { zokou } = require('../framework/zokou');
+const { ajouterUtilisateurAvecWarnCount, getWarnCountByJID, resetWarnCountByJID } = require('../bdd/mute');
+const s = require("../set");
 
-zokou( 
-  { 
-    nomCom : 'mute', 
-    categorie : 'Group' 
-  },
-  async (dest,zk,commandeOptions) => {
-    const {ms , arg, repondre,superUser,verifGroupe,verifAdmin , msgRepondu , auteurMsgRepondu} = commandeOptions;
-    if(!verifGroupe ) {repondre('this is a group commands') ; return};
-    if(verifAdmin || superUser) {
-      if(!msgRepondu){repondre('reply a message of user to mute'); return};
-      await muteUser(auteurMsgRepondu)
-      repondre(`@${auteurMsgRepondu.split('@')[0]} has been muted`, {mentions: [auteurMsgRepondu]})
-    } else {
-      repondre('you are not admin')
+zokou(
+    {
+        nomCom: 'mute', // Changed from 'warn' to 'mute'
+        categorie: 'Group'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { ms, arg, repondre, superUser, verifGroupe, verifAdmin, msgRepondu, auteurMsgRepondu } = commandeOptions;
+        
+        if (!verifGroupe) { repondre('This is a group command'); return; }
+
+        if (verifAdmin || superUser) {
+            if (!msgRepondu) { repondre('Reply to a message of user to mute'); return; }
+            
+            // Mute logic
+            try {
+                // Add your mute role logic here
+                // Example: await zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "mute");
+                repondre(`User has been muted.`);
+            } catch (error) {
+                repondre('Error muting user: ' + error);
+            }
+        } else {
+            repondre('You are not admin');
+        }
     }
-  }
 );
 
-zokou( 
-  { 
-    nomCom : 'unmute', 
-    categorie : 'Group' 
-  },
-  async (dest,zk,commandeOptions) => {
-    const {ms , arg, repondre,superUser,verifGroupe,verifAdmin , msgRepondu , auteurMsgRepondu} = commandeOptions;
-    if(!verifGroupe ) {repondre('this is a group commands') ; return};
-    if(verifAdmin || superUser) {
-      if(!msgRepondu){repondre('reply a message of user to unmute'); return};
-      await unmuteUser(auteurMsgRepondu)
-      repondre(`@${auteurMsgRepondu.split('@')[0]} has been unmuted`, {mentions: [auteurMsgRepondu]})
-    } else {
-      repondre('you are not admin')
+zokou(
+    {
+        nomCom: 'unmute', // New unmute command
+        categorie: 'Group'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { ms, arg, repondre, superUser, verifGroupe, verifAdmin, msgRepondu, auteurMsgRepondu } = commandeOptions;
+        
+        if (!verifGroupe) { repondre('This is a group command'); return; }
+
+        if (verifAdmin || superUser) {
+            if (!msgRepondu) { repondre('Reply to a message of user to unmute'); return; }
+            
+            // Unmute logic
+            try {
+                // Add your unmute role logic here
+                // Example: await zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "unmute");
+                repondre(`User has been unmuted.`);
+            } catch (error) {
+                repondre('Error unmuting user: ' + error);
+            }
+        } else {
+            repondre('You are not admin');
+        }
     }
-  }
 );
