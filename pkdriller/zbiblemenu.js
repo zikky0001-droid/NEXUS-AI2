0e0080071d6b5a6143748f4ede6343101a17ef2b
Mejja <pkdrillerbusiness@gmail.com>
2025-07-04 20:31:33 +0300

    Update zbiblemenu.js

--- pkdriller/zbiblemenu.js
+++ pkdriller/zbiblemenu.js
@@ -131,7 +131,7 @@ let menuMsg = `
                 externalAdReply: {
                     title: "*NEXUS-AI* HOLY BIBLE LIST",
                     body: "Live with God my friends you don't know your tomorrow",
-                    thumbnailUrl: ".https://files.catbox.moe/pdhcob.jpeg",
+                    thumbnailUrl: "https://github.com/pkdriller0/NEXUS-XMD-DATA/raw/refs/heads/main/logo/bible.jpeg",
                     sourceUrl: "https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x",
                     mediaType: 1,
                     renderLargerThumbnail: true
